149d40f86b5525ca088285ed4c375cb0b65acdeb
Konstantin Lukashkin <lukkos62@gmail.com>
2023-12-19 07:02:14 +0500

    lint regex

--- 20/20_reassessment.py
+++ 20/20_reassessment.py
@@ -1,22 +1,24 @@
 from collections import defaultdict
 
-with open('input', 'r') as fd:
-    pattern = fd.readline().rstrip().strip('^$')
+with open("input", "r") as fd:
+    pattern = fd.readline().rstrip().strip("^$")
 depths: dict[complex] = defaultdict(lambda: float("inf"))
 depths[0 + 0j] = 0
 stack = []
 position = 0 + 0j
-step = {'N': -1, 'W': -1j, 'S': 1, 'E': 1j}
+step = {"N": -1, "W": -1j, "S": 1, "E": 1j}
 for char in pattern:
     match char:
-        case '(':
+        case "(":
             stack.append(position)
-        case ')':
+        case ")":
             position = stack.pop()
-        case '|':
+        case "|":
             position = stack[-1]
         case _:
-            depths[position + step[char]] = min(depths[position] + 1, depths[position + step[char]])
+            depths[position + step[char]] = min(
+                depths[position] + 1, depths[position + step[char]]
+            )
             position += step[char]
 
 print("Max depth:", max(depths.values()))
